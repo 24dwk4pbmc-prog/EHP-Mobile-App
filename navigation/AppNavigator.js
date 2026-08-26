a24dcdf4d3382ee7b38025f301629d8f231bc4a0
24dwk4pbmc-prog <24dwk4pbmc@privaterelay.appleid.com>
2026-08-26 10:03:00 +1000

    Update AppNavigator.js

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,6 +1,52 @@
-import HomeScreen from "../screens/HomeScreen";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import LoginScreen from "../screens/LoginScreen";
-<Stack.Screen
-  name="Login"
-  component={LoginScreen}
-/>
+import HomeScreen from "../screens/HomeScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+import NutritionScreen from "../screens/NutritionScreen";
+import TrainingScreen from "../screens/TrainingScreen";
+import ProgressScreen from "../screens/ProgressScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+        />
+
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+        />
+
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+        />
+
+        <Stack.Screen
+          name="Nutrition"
+          component={NutritionScreen}
+        />
+
+        <Stack.Screen
+          name="Training"
+          component={TrainingScreen}
+        />
+
+        <Stack.Screen
+          name="Progress"
+          component={ProgressScreen}
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
